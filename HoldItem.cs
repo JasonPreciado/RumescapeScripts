250eb06c37bc1b0b37546d06d3e1025b8b6d396a
JasonPreciado <JasonPreciado@users.noreply.github.com>
2020-03-07 18:56:44 -0800

    Added HoldItem and PlayerController scripts

---
+++ HoldItem.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class HoldItem : MonoBehaviour
+{
+    //TODO: Use LERP on rotation, reformat
+    
+    public Transform destinationTransform;
+    public Material newMaterial;
+    public float dist;
+
+    private float holdDist;
+
+    Camera cam;
+    Rigidbody rb;
+    private Transform originalParent;
+    private bool beingHeld = false;
+    Renderer m_Renderer;
+    Material temp;
+    private float radius = 1f;
+
+    private float tweenDuration = 0.1f;
+    private Vector3 pickupStartPosition, pickupEndPosition;
+    private float pickupStartTime, pickupEndTime;
+    public bool animatePickUp = false;
+    private GameObject heldItem;
+
+    private float throwForceXY = 1f;
+    private float throwForceZ = 1f;
+
+    private int collision = 0;
+
+    void Start()
+    {
+        originalParent = transform.parent;
+        m_Renderer = GetComponent<MeshRenderer>();
+        temp = m_Renderer.material;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerController>().cam;
+    }
+
+
+    void Update()
+    {
+        dist = Vector3.Distance(cam.transform.position, transform.position);
+        TriggerOutline();
+
+        if(beingHeld)
+        {
+            holdDist = Vector3.Distance(destinationTransform.position, heldItem.transform.position);
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            if (holdDist > 0.2f && !animatePickUp)
+            {
+                rb.useGravity = true;
+                heldItem.transform.parent = originalParent;
+                beingHeld = false;
+            }
+        }
+
+
+        if (animatePickUp == true)
+        { //Animating the travel between both points
+            if (Time.time < pickupEndTime)
+            {
+                float lerp = Mathf.InverseLerp(pickupStartTime, pickupEndTime, Time.time);    // Calculate lerp value
+                heldItem.transform.localPosition = Vector3.Lerp(pickupStartPosition, pickupEndPosition, lerp); //Animation call
+            }
+            else
+            {
+                //Animation finished
+                animatePickUp = false;
+            
+            }
+        }
+    }
+
+    public void ItemInteraction(GameObject item)
+    { 
+
+        if (!beingHeld && dist <= radius)
+        {
+            heldItem = item;
+            rb = heldItem.GetComponent<Rigidbody>();
+            //item.transform.position = destinationTransform.position;
+            heldItem.transform.parent = destinationTransform;
+            //rb.isKinematic = true;
+            rb.useGravity = false;
+            rb.detectCollisions = true;
+           
+            beingHeld = true;
+            animatePickUp = true;
+            pickupStartTime = Time.time;                         // Store when we start
+            pickupEndTime = Time.time + tweenDuration;
+
+            pickupStartPosition = item.transform.localPosition;  // Store where we start
+            pickupEndPosition = new Vector3(0, 0, 0);
+        }
+        else if (beingHeld)
+        {
+            //rb.isKinematic = false;
+            rb.useGravity = true;
+            heldItem.transform.parent = originalParent;
+            beingHeld = false;
+        }  
+    }
+
+    public void throwItem(GameObject item, float swipeTime, Vector2 swipeDirection)
+    {
+        Debug.Log("Throw");
+        //rb.isKinematic = false;
+        rb.useGravity = true;
+        //rb.AddForce(swipeDirection.x * throwForceXY,swipeDirection.y * throwForceXY,throwForceZ/swipeTime);
+        rb.AddForce(destinationTransform.forward * 5, ForceMode.Impulse);
+        rb.AddForce(destinationTransform.up * 5, ForceMode.Impulse);
+        heldItem.transform.parent = originalParent;
+        beingHeld = false;
+    }
+
+    void TriggerOutline()
+    {
+        if (dist <= radius && !beingHeld)
+        {
+            m_Renderer.material = newMaterial;
+        }
+        else
+        {
+            m_Renderer.material = temp;
+        }
+    }
+    void OnCollisionEnter()
+    {
+        if(beingHeld)
+        {
+            collision++;
+            Debug.Log("Collisions:" + collision);
+        }
+    }
+    void OnCollisionExit()
+    {
+        if (beingHeld)
+        {
+            //heldItem.transform.localPosition = new Vector3(0, 0, 0);
+            animatePickUp = true;
+            pickupStartTime = Time.time;                         // Store when we start
+            pickupEndTime = Time.time + tweenDuration;
+
+            pickupStartPosition = heldItem.transform.localPosition;  // Store where we start
+            pickupEndPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    private Vector2 touchPosition = default;
+    GameObject item;
+    GameObject Orb;
+    string name;
+
+    private Vector2 startTouchPos, endTouchPos, swipeDist;
+    private float startTime, endTime, swipeTime;
+
+    private bool isSwipe = false;
+    private bool isTouch = false;
+
+    private bool isHoldingItem = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPos = Input.GetTouch(0).position;
+            startTime = Time.time;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPos = Input.GetTouch(0).position;
+            endTime = Time.time;
+            swipeTime = endTime - startTime;
+            swipeDist = endTouchPos - startTouchPos;
+
+            if (swipeDist.y / Screen.height >= 0.25 && swipeTime < 1)
+            {
+                isSwipe = true;
+            }
+            else if (swipeTime < 0.25)
+            {
+                isTouch = true;
+            }
+        }
+
+
+        if (isSwipe) //isSwipe
+        {
+            if (isHoldingItem)
+            {
+                item.GetComponent<HoldItem>().throwItem(item, swipeTime, swipeDist);
+                isHoldingItem = false;
+            }
+            isSwipe = false;
+        }//if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        else if(isTouch) //isTouch
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (isHoldingItem)
+            {
+                item.GetComponent<HoldItem>().ItemInteraction(item);
+                isHoldingItem = false;
+            }
+            else if (Physics.Raycast(ray, out hit, 5))
+            {
+                //Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (hit.transform.tag == "Orb")
+                {
+                    Orb = hit.collider.gameObject;
+                    Orb.GetComponent<ToggleOrb>().LightOrb(Orb);
+                }
+                else if (hit.transform.tag == "button")
+                {
+                    GameObject control = GameObject.Find("RotatingMaze");
+                    name = hit.collider.gameObject.name;
+
+                    control.GetComponent<Rotate>().activate(name);
+
+                }
+                else if (hit.transform.tag == "LockNum")
+                {
+                    Debug.Log("HIT");
+                    GameObject safe = GameObject.Find("Safe");
+                    name = hit.collider.gameObject.name;
+
+                    safe.GetComponent<Safe>().changeLockNumber(name);
+
+                }
+                else if (hit.collider.gameObject.GetComponent<HoldItem>() != null)
+                {
+                    item = hit.collider.gameObject;
+                    item.GetComponent<HoldItem>().ItemInteraction(item);
+                    isHoldingItem = true;
+                }
+                else
+                {
+                    item = hit.collider.gameObject;
+                    //Debug.Log("got here" + item.name);
+                    if (item != null)
+                        item.GetComponent<ItemPickup>().PickUp(item);
+                }
+            }
+            isTouch = false;
+        }
+    }
+}
+
